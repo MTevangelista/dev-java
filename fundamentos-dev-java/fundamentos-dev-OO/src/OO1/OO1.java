2ed95cb05cfca545eaa1338eea5463d5552e7a00
Matheus Evangelista <matheusevangelistadev@gmail.com>
2021-05-02 20:52:26 -0300

    Java dev fundamentals: Add object orientation exercise examples

---
+++ fundamentos-dev-java/fundamentos-dev-OO/src/OO1/OO1.java
@@ -0,0 +1,11 @@
+package OO1;
+
+public class OO1 {
+    public static void main(String[] args) {
+        Conta c1 = new Conta(1, "Matheus", 100);  
+        Conta c2 = new Conta(100, "Mthe", 1000);
+        
+        System.out.println(c1);
+        System.out.println(c2);
+    }
+}
